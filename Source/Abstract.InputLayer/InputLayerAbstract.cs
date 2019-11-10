cd3cf6692530fd7d084cbdd8599535c86cc82e25
Iv-Umlore <iv.kiselev1998@yandex.ru>
2019-11-10 13:28:52 +0300

    Добавлен абстрактный класс для входных слоёв

---
+++ Source/Abstract.InputLayer/InputLayerAbstract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.InputLayer
+{
+    public abstract class InputLayerAbstract
+    {
+        private int _inputParametersCount;
+        private int _outputParametersCount;
+
+        public InputLayerAbstract(int inputParametersCount, int outputParametersCount)
+        {
+            _inputParametersCount = inputParametersCount;
+            _outputParametersCount = outputParametersCount;
+        }
+
+
+        public virtual List<double> GetValueForNetwork(List<int> values) {
+            return new List<double>();
+        }
+    }
+
+}
